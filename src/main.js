85c329e5c7e4e6a5a921c39666aa2a4638ef1d34
yumaojun <719118794@qq.com>
2019-07-07 13:47:58 +0800

    axios upgrade to 0.19

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@ import router from './router'
 import './icons' // icon
 import './permission' // permission control
 
-
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -33,7 +32,6 @@ if (process.env.NODE_ENV === 'production') {
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
 })
-
 
 Vue.config.productionTip = false
 
