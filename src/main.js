fb39621b1f56adea6067e94bda9134200d44205a
yumaojun <maojun.yu@56qq.com>
2019-07-09 13:51:53 +0800

    添加样式

--- src/main.js
+++ src/main.js
@@ -16,6 +16,8 @@ import router from './router'
 import './icons' // icon
 import './permission' // permission control
 
+import * as filters from './filters' // global filters
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -31,6 +33,11 @@ if (process.env.NODE_ENV === 'production') {
 
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium' // set element-ui default size
+})
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
 })
 
 Vue.config.productionTip = false
